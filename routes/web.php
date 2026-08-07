6c712fa882a35cf2b1dc4d7d48c2d5b44146a24c
Achmad Fikri Hidayatullah <refikuri@gmail.com>
2026-08-07 10:52:01 +0700

    Add temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrate-xyz123', function () {
+    if (request('key') !== 'rahasia-portal-berita-2026') {
+        abort(403);
+    }
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration berhasil dijalankan: ' . Artisan::output();
+});
 
 require base_path('routes/auth.php');
 require base_path('routes/front.php');

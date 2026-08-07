4de477d63f602c83b8c9f6e8591791138eb62025
Achmad Fikri Hidayatullah <refikuri@gmail.com>
2026-08-07 10:56:46 +0700

    Add temporary seeder route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-seed-abc789', function () {
+    if (request('key') !== 'rahasia-seeder-portal-2026') {
+        abort(403);
+    }
+    Artisan::call('db:seed', ['--force' => true]);
+    return 'Seeder berhasil dijalankan: ' . Artisan::output();
+});
 
 require base_path('routes/auth.php');
 require base_path('routes/front.php');
